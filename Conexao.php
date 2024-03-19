66a5c24f6625c804a64ddd9718c5382b7fb31d59
VictorVicente004 <vicentevictor2004@gmail.com>
2024-03-19 00:43:59 -0300

    atualizarconexao

--- Conexao.php
+++ Conexao.php
@@ -4,7 +4,7 @@ class Conexao
     static function conectar()
     {
         //iniciando a conexão com mysql e o bd
-        $con = new PDO("mysql:host=localhost;port=3306;dbname=meubancodedados","root","usbw"); 
+        $con = new PDO("mysql:host=localhost;port=3306;dbname=menudeclientes","victor","@Victor2004"); 
         //ativando exibição de erros do PDO
         $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         return $con; //retornando a conexão com o BD
